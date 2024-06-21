11d705f973695fdf2f40beac8f9cf35b6141ff3c
NIKHIL CM <softmantk@users.noreply.github.com>
2024-06-21 15:00:12 +0530

    improve message

--- index.js
+++ index.js
@@ -13,26 +13,28 @@ console.log('@@@ :DOWNLOAD_PATH: ', DOWNLOAD_PATH);
 
 app.use(express.json());
 
-const sendMessage = async (chatId, text) => {
-    const url = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`;
+const sendMessage = ( chatId ) => async ( text ) => {
+    const url = `https://api.telegram.org/bot${ BOT_TOKEN }/sendMessage`;
     await axios.post(url, {
         chat_id: chatId,
         text: text
     });
 };
 
-app.post(`/webhook/${BOT_TOKEN}`, async (req, res) => {
+app.post(`/webhook/${ BOT_TOKEN }`, async ( req, res ) => {
     const message = req.body.message;
     console.log("message: ", JSON.stringify(message, null, 2));
-
     if (message && message.document) {
         const fileId = message.document.file_id;
         const originalFileName = message.document.file_name;
         const chatId = message.chat.id;
+        const reply = sendMessage(chatId)
 
         try {
             // Get file path
-            const fileUrl = `${BASE_URL}/bot${BOT_TOKEN}/getFile?file_id=${fileId}`;
+            await reply('getting your file..')
+            const fileUrl = `${ BASE_URL }/bot${ BOT_TOKEN }/getFile?file_id=${ fileId }`;
+            await reply('file downloaded..')
             console.log("fileurl: ", fileUrl);
             const fileResponse = await axios.get(fileUrl);
             const filePath = fileResponse.data.result.file_path;
@@ -40,26 +42,27 @@ app.post(`/webhook/${BOT_TOKEN}`, async (req, res) => {
             const sourcePath = filePath;
             const destinationPath = path.join(DOWNLOAD_PATH, originalFileName);
 
-            console.log('@@@ :destinationPath: ', destinationPath);
+            await reply('moving to plex media')
 
             if (fs.existsSync(sourcePath)) {
-                fs.rename(sourcePath, destinationPath, async (err) => {
+                fs.rename(sourcePath, destinationPath, async ( err ) => {
                     if (err) {
                         console.error('Error moving file:', err);
-                        await sendMessage(chatId, 'Error moving file.');
+                        await reply('Error moving file')
                         res.status(500).send('Error moving file.');
                     } else {
-                        console.log(`File moved to ${destinationPath}`);
-                        await sendMessage(chatId, 'File uploaded to the server successfully.');
+                        console.log(`File moved to ${ destinationPath }`);
+                        await reply('moved file successfully')
                         res.send('File uploaded to the server successfully.');
                     }
                 });
             } else {
                 console.error('File does not exist at source path:', sourcePath);
-                await sendMessage(chatId, 'File not found.');
+                await reply('File not found')
                 res.status(404).send('File not found.');
             }
-        } catch (error) {
+        }
+        catch (error) {
             console.error('Error:', error && error.response ? error.response.data : error.toString());
             await sendMessage(chatId, 'Error processing file.');
             res.send('Error processing file.');
@@ -69,12 +72,12 @@ app.post(`/webhook/${BOT_TOKEN}`, async (req, res) => {
     }
 });
 
-app.get("*", (req, res) => {
+app.get("*", ( req, res ) => {
     console.log("Not webhook");
     res.send("Hello world");
     return;
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${ PORT }`);
 });
